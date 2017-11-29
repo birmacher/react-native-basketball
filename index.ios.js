0578fd360ce286e4f5ecc8a9fcc7b219a90f2f48
Barnabas Birmacher <birmacher@gmail.com>
2017-11-29 12:41:37 +0100

    React native updated to 0.50.4 Xcode team added

--- index.ios.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {
-  AppRegistry,
-} from 'react-native';
-
-import Basketball from './Basketball';
-
-AppRegistry.registerComponent('Basketball', () => Basketball);
